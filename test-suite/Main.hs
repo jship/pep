922a7545d37bdf0d64a337c34754bb9b6bb91765
Jason Shipman <jasonpshipman@gmail.com>
2017-06-26 19:30:51 -0400

    Add tasty-auto

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -1,12 +1,1 @@
-import qualified Test.Tasty
-import Test.Tasty.Hspec
-
-main :: IO ()
-main = do
-    test <- testSpec "pep" spec
-    Test.Tasty.defaultMain test
-
-spec :: Spec
-spec = parallel $ do
-    it "is trivially true" $ do
-        True `shouldBe` True
+{-# OPTIONS_GHC -F -pgmF tasty-auto #-}
---
+++ test-suite/Pep/BaseTest.hs
@@ -0,0 +1,4 @@
+module Pep.BaseTest where
+
+prop_Addition_is_commutative :: Int -> Int -> Bool
+prop_Addition_is_commutative a b = a + b == b + a
