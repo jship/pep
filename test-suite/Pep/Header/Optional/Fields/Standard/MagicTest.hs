4a6049fb6770ed83314f670a7c8376cd9b8731ae
Jason Shipman <jasonpshipman@gmail.com>
2017-07-07 22:30:26 -0400

    Minor rename to property test

--- test-suite/Pep/Header/Optional/Fields/Standard/MagicTest.hs
+++ test-suite/Pep/Header/Optional/Fields/Standard/MagicTest.hs
@@ -7,8 +7,8 @@ import Pep.Header.Optional.Fields.Standard.Magic
 import qualified Data.Serialize as Cereal
 import Test.Tasty.QuickCheck (Arbitrary(..), arbitraryBoundedEnum)
 
-prop_Round_trip_for_optional_header_magic_number :: Magic -> Bool
-prop_Round_trip_for_optional_header_magic_number magic =
+prop_Round_trip_for_optional_header_standard_fields_magic_number :: Magic -> Bool
+prop_Round_trip_for_optional_header_standard_fields_magic_number magic =
   Cereal.decode (Cereal.encode magic) == Right magic
 
 instance Arbitrary Magic where
