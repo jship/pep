8e77964fbac3a16c42a0cd4686bec69940669dce
Jason Shipman <jasonpshipman@gmail.com>
2017-07-15 17:12:36 -0400

    Remove Serialize instance from StandardFields as StandardFields and WindowsFields need to be de/serialized in same put/get

--- library/Pep/Header/Optional/Fields/Standard.hs
+++ library/Pep/Header/Optional/Fields/Standard.hs
@@ -1,13 +1,9 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Pep.Header.Optional.Fields.Standard
   ( StandardFields(..)
   ) where
 
 import Pep.Header.Optional.Fields.Standard.Magic
 
-import Data.Serialize (Serialize)
-import qualified Data.Serialize as Cereal
 import Data.Word (Word8, Word32)
 
 data StandardFields = StandardFields
@@ -22,31 +18,7 @@ data StandardFields = StandardFields
   , standardFieldsBaseOfData              :: Maybe Word32 -- only exists if PE32 and not PE32+
   } deriving (Eq, Show)
 
-instance Serialize StandardFields where
-  get = do
-    standardFieldsMagic <- Cereal.get
-    standardFieldsMajorLinkerVersion <- Cereal.getWord8
-    standardFieldsMinorLinkerVersion <- Cereal.getWord8
-    standardFieldsSizeOfCode <- Cereal.getWord32le
-    standardFieldsSizeOfInitializedData <- Cereal.getWord32le
-    standardFieldsSizeOfUninitializedData <- Cereal.getWord32le
-    standardFieldsAddressOfEntryPoint <- Cereal.getWord32le
-    standardFieldsBaseOfCode <- Cereal.getWord32le
-    if standardFieldsMagic == PE32
-      then do
-        standardFieldsBaseOfData <- Just <$> Cereal.getWord32le
-        pure $ StandardFields{..}
-      else do
-        let standardFieldsBaseOfData = Nothing
-        pure $ StandardFields{..}
-
-  put StandardFields{..} = do
-    Cereal.put standardFieldsMagic
-    Cereal.putWord8 standardFieldsMajorLinkerVersion
-    Cereal.putWord8 standardFieldsMinorLinkerVersion
-    Cereal.putWord32le standardFieldsSizeOfCode
-    Cereal.putWord32le standardFieldsSizeOfInitializedData
-    Cereal.putWord32le standardFieldsSizeOfUninitializedData
-    Cereal.putWord32le standardFieldsAddressOfEntryPoint
-    Cereal.putWord32le standardFieldsBaseOfCode
-    maybe (pure ()) Cereal.putWord32le standardFieldsBaseOfData
+-- Note: No Serialize instance is provided here as parsing WindowsFields
+-- depends on the value of standardFieldsMagic. Instead, an aggregate
+-- Serialize instance is provided in Pep.Header.Optional.Fields that
+-- handles both StandardFields and WindowsFields in one shot.
